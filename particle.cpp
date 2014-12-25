5e6666b44e521abc41f9910d259eb0979ddb1c5d
wiekonek <wiekonek@gmail.com>
2014-12-25 19:04:17 +0100

    particles can move

--- particle.cpp
+++ particle.cpp
@@ -1,26 +1,40 @@
 #include "particle.h"
 
-Particle::Particle()
-{
+using world_of_particles::Particle;
+using world_of_particles::Position;
+using world_of_particles::Direction;
+using std::ofstream;
 
+Particle::Particle(MultiVal* max, string name, Position* position, Direction* direction, double r) {
+  this->max = new MultiVal(max->get_x(), max->get_y());
+  this->name = name;
+  this->position = new Position(position);
+  this->direction = new Direction(direction);
+  this->r = r;
 }
 
-Particle::Particle(const Particle& other)
-{
-
+Particle::~Particle() {
+  delete position;
+  delete direction;
 }
 
-Particle::~Particle()
-{
-
+void Particle::draw(string type) {
+  cout<<name<<"\t"<<type<<"\t"<<position->get_x()<<"\t"<<position->get_y()<<"\t"<<r;
 }
 
-Particle& Particle::operator=(const Particle& other)
-{
-
+void Particle::save(string type, double add) {
+  ofstream file;
+  file.open("output.txt", std::ios::out | std::ios::app);
+//   file.precision(20);
+  file << name << "\t" << type << "\t" << position->get_x() << "\t" << position->get_y() << "\t" << r << "\t" << add << "\n";
+  file.close();
 }
 
-bool Particle::operator==(const Particle& other) const
-{
 
+void Particle::move() {
+  if(position->get_x() < 0 || position->get_x() > max->get_x()) direction->invert_x();
+  if(position->get_y() < 0 || position->get_y() > max->get_y()) direction->invert_y();
+  position->update(direction);
 }
+
+
